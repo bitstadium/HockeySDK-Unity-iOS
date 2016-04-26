244b029d4a3614078146aacac4fc03633a9ad932
chrwend <chwend@microsoft.com>
2016-04-26 14:56:49 -0700

    Reformat script

--- ExampleGame/Assets/HockeyAppUnityIOS/HockeyAppUnity-Scripts/HockeyAppIOS.cs
+++ ExampleGame/Assets/HockeyAppUnityIOS/HockeyAppUnity-Scripts/HockeyAppIOS.cs
@@ -1,41 +1,43 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using System;
 using System.IO;
 using System.Runtime.InteropServices;
 
-public class HockeyAppIOS : MonoBehaviour {
+public class HockeyAppIOS : MonoBehaviour
+{
 
 	protected const string HOCKEYAPP_BASEURL = "https://rink.hockeyapp.net/";
 	protected const string HOCKEYAPP_CRASHESPATH = "api/2/apps/[APPID]/crashes/upload";
 	protected const string LOG_FILE_DIR = "/logs/";
 	protected const int MAX_CHARS = 199800;
 
-	public enum AuthenticatorType {
+	public enum AuthenticatorType
+	{
 		Anonymous,
 		Device,
 		HockeyAppUser,
 		HockeyAppEmail,
 		WebAuth
 	}
-	
-	[Header("HockeyApp Setup")]
+
+	[Header ("HockeyApp Setup")]
 	public string appID = "your-hockey-app-id";
 	public string serverURL = "your-custom-server-url";
 
-	[Header("Authentication")]
+	[Header ("Authentication")]
 	public AuthenticatorType authenticatorType;
 	public string secret = "your-hockey-app-secret";
 
-	[Header("Crashes & Exceptions")]
+	[Header ("Crashes & Exceptions")]
 	public bool autoUploadCrashes = false;
 	public bool exceptionLogging = true;
 
-	[Header("Metrics")]
+	[Header ("Metrics")]
 	public bool userMetrics = true;
 
-	[Header("Version Updates")]
+	[Header ("Version Updates")]
 	public bool updateAlert = true;
 
 	#if (UNITY_IPHONE && !UNITY_EDITOR)
@@ -52,17 +54,17 @@ public class HockeyAppIOS : MonoBehaviour {
 	[DllImport("__Internal")]
 	private static extern string HockeyApp_GetSdkName();
 	#endif
-	
-	void Awake(){
-		
+
+	void Awake ()
+	{
+
 		#if (UNITY_IPHONE && !UNITY_EDITOR)
 		DontDestroyOnLoad(gameObject);
+		CreateLogDirectory();
 
-		if(exceptionLogging == true && IsConnected() == true)
-		{
+		if (exceptionLogging == true && IsConnected() == true) {
 			List<string> logFileDirs = GetLogFiles();
-			if ( logFileDirs.Count > 0)
-			{
+			if (logFileDirs.Count > 0) {
 				Debug.Log("Found files: " + logFileDirs.Count);
 				StartCoroutine(SendLogs(logFileDirs));
 			}
@@ -70,27 +72,28 @@ public class HockeyAppIOS : MonoBehaviour {
 		#endif
 	}
 
-	void OnEnable(){
-		
+	void OnEnable ()
+	{
 		#if (UNITY_IPHONE && !UNITY_EDITOR)
-		if(exceptionLogging == true){
+		if (exceptionLogging == true) {
 			System.AppDomain.CurrentDomain.UnhandledException += OnHandleUnresolvedException;
 			Application.logMessageReceived += OnHandleLogCallback;
 		}
 		#endif
 	}
 
-	void OnDisable(){
+	void OnDisable ()
+	{
 		#if (UNITY_IPHONE && !UNITY_EDITOR)
-		if(exceptionLogging == true){
+		if (exceptionLogging == true) {
 			System.AppDomain.CurrentDomain.UnhandledException -= OnHandleUnresolvedException;
 			Application.logMessageReceived -= OnHandleLogCallback;
 		}
 		#endif
 	}
 
-	void GameViewLoaded(string message) { 
-
+	void GameViewLoaded (string message)
+	{
 		#if (UNITY_IPHONE && !UNITY_EDITOR)
 		string urlString = GetBaseURL();
 		string authTypeString = GetAuthenticatorTypeString();
@@ -102,13 +105,14 @@ public class HockeyAppIOS : MonoBehaviour {
 	/// Collect all header fields for the custom exception report.
 	/// </summary>
 	/// <returns>A list which contains the header fields for a log file.</returns>
-	protected virtual List<string> GetLogHeaders() {
-		List<string> list = new List<string>();
-		
+	protected virtual List<string> GetLogHeaders ()
+	{
+		List<string> list = new List<string> ();
+
 		#if (UNITY_IPHONE && !UNITY_EDITOR)
 		string bundleID = HockeyApp_GetBundleIdentifier();
 		list.Add("Package: " + bundleID);
-		
+
 		string versionCode = HockeyApp_GetVersionCode();
 		list.Add("Version Code: " + versionCode);
 
@@ -117,12 +121,12 @@ public class HockeyAppIOS : MonoBehaviour {
 
 		string osVersion = "OS: " + SystemInfo.operatingSystem.Replace("iPhone OS ", "");
 		list.Add (osVersion);
-		
+
 		list.Add("Model: " + SystemInfo.deviceModel);
 
 		list.Add("Date: " + DateTime.UtcNow.ToString("ddd MMM dd HH:mm:ss {}zzzz yyyy").Replace("{}", "GMT"));
 		#endif
-		
+
 		return list;
 	}
 
@@ -131,53 +135,42 @@ public class HockeyAppIOS : MonoBehaviour {
 	/// </summary>
 	/// <param name="log">A string that contains information about the exception.</param>
 	/// <returns>The form data for the current exception report.</returns>
-	protected virtual WWWForm CreateForm(string log){
-		
-		WWWForm form = new WWWForm();
+	protected virtual WWWForm CreateForm (string log)
+	{
+
+		WWWForm form = new WWWForm ();
 
 		#if (UNITY_IPHONE && !UNITY_EDITOR)
 		byte[] bytes = null;
-		using(FileStream fs = File.OpenRead(log)){
+		using(FileStream fs = File.OpenRead(log)) {
 
-			if (fs.Length > MAX_CHARS)
-			{
+			if (fs.Length > MAX_CHARS) {
 				string resizedLog = null;
 
-				using(StreamReader reader = new StreamReader(fs)){
-
+				using(StreamReader reader = new StreamReader(fs)) {
 					reader.BaseStream.Seek( fs.Length - MAX_CHARS, SeekOrigin.Begin );
 					resizedLog = reader.ReadToEnd();
 				}
 
 				List<string> logHeaders = GetLogHeaders();
 				string logHeader = "";
-					
-				foreach (string header in logHeaders)
-				{
+
+				foreach (string header in logHeaders) {
 					logHeader += header + "\n";
 				}
-					
+
 				resizedLog = logHeader + "\n" + "[...]" + resizedLog;
 
-				try
-				{
+				try {
 					bytes = System.Text.Encoding.Default.GetBytes(resizedLog);
-				}
-				catch(ArgumentException ae)
-				{
+				} catch (ArgumentException ae) {
 					if (Debug.isDebugBuild) Debug.Log("Failed to read bytes of log file: " + ae);
 				}
-			}
-			else
-			{
-				try
-				{
+			} else {
+				try {
 					bytes = File.ReadAllBytes(log);
-				}
-				catch(SystemException se)
-				{
-					if (Debug.isDebugBuild) 
-					{
+				} catch (SystemException se) {
+					if (Debug.isDebugBuild) {
 						Debug.Log("Failed to read bytes of log file: " + se);
 					}
 				}
@@ -185,54 +178,55 @@ public class HockeyAppIOS : MonoBehaviour {
 			}
 		}
 
-		if(bytes != null)
-		{
+		if (bytes != null) {
 			form.AddBinaryData("log", bytes, log, "text/plain");
 		}
-
 		#endif
-		
+
 		return form;
+	}
+
+	/// <summary>
+	/// Create the log directory if needed.
+	/// </summary>
+	protected virtual void CreateLogDirectory ()
+	{
+		#if (UNITY_IPHONE && !UNITY_EDITOR)
+		string logsDirectoryPath = Application.persistentDataPath + LOG_FILE_DIR;
+
+		try {
+			Directory.CreateDirectory (logsDirectoryPath);
+		} catch (Exception e) {
+			if (Debug.isDebugBuild) Debug.Log ("Failed to create log directory at " + logsDirectoryPath + ": " + e);
+		}
+		#endif
 	}
 
 	/// <summary>
 	/// Get a list of all existing exception reports.
 	/// </summary>
 	/// <returns>A list which contains the filenames of the log files.</returns>
-	protected virtual List<string> GetLogFiles() {
-
-		List<string> logs = new List<string>();
+	protected virtual List<string> GetLogFiles ()
+	{
+		List<string> logs = new List<string> ();
 
 		#if (UNITY_IPHONE && !UNITY_EDITOR)
 		string logsDirectoryPath = Application.persistentDataPath + LOG_FILE_DIR;
 
-		try
-		{
-			if (Directory.Exists(logsDirectoryPath) == false)
-			{
-				Directory.CreateDirectory(logsDirectoryPath);
-			}
-		
+		try {
 			DirectoryInfo info = new DirectoryInfo(logsDirectoryPath);
 			FileInfo[] files = info.GetFiles();
 
-			if (files.Length > 0)
-			{
-				foreach (FileInfo file in files)
-				{
-					if (file.Extension == ".log")
-					{
+			if (files.Length > 0) {
+				foreach (FileInfo file in files) {
+					if (file.Extension == ".log") {
 						logs.Add(file.FullName);
-					}
-					else
-					{
+					} else {
 						File.Delete(file.FullName);
 					}
 				}
 			}
-		}
-		catch(Exception e)
-		{
+		} catch (Exception e) {
 			if (Debug.isDebugBuild) Debug.Log("Failed to write exception log to file: " + e);
 		}
 		#endif
@@ -243,10 +237,10 @@ public class HockeyAppIOS : MonoBehaviour {
 	/// <summary>
 	/// Upload existing reports to HockeyApp and delete them locally.
 	/// </summary>
-	protected virtual IEnumerator SendLogs(List<string> logs){
-
+	protected virtual IEnumerator SendLogs (List<string> logs)
+	{
 		string crashPath = HOCKEYAPP_CRASHESPATH;
-		string url = GetBaseURL() + crashPath.Replace("[APPID]", appID);
+		string url = GetBaseURL () + crashPath.Replace ("[APPID]", appID);
 
 		#if (UNITY_IPHONE && !UNITY_EDITOR)
 		string sdkVersion = HockeyApp_GetSdkVersion ();
@@ -261,20 +255,17 @@ public class HockeyAppIOS : MonoBehaviour {
 			WWWForm postForm = CreateForm (log);
 			string lContent = postForm.headers ["Content-Type"].ToString ();
 			lContent = lContent.Replace ("\"", "");
-			Dictionary<string,string> headers = new Dictionary<string,string> ();
+			Dictionary<string, string> headers = new Dictionary<string, string> ();
 			headers.Add ("Content-Type", lContent);
 			WWW www = new WWW (url, postForm.data, headers);
 			yield return www;
 
-			if (String.IsNullOrEmpty (www.error)) 
-			{
-				try 
-				{
+			if (String.IsNullOrEmpty (www.error)) {
+				try {
 					File.Delete (log);
-				} 
-				catch (Exception e) 
-				{
-					if (Debug.isDebugBuild) Debug.Log ("Failed to delete exception log: " + e);
+				} catch (Exception e) {
+					if (Debug.isDebugBuild)
+						Debug.Log ("Failed to delete exception log: " + e);
 				}
 			}
 		}
@@ -285,27 +276,24 @@ public class HockeyAppIOS : MonoBehaviour {
 	/// </summary>
 	/// <param name="logString">A string that contains the reason for the exception.</param>
 	/// <param name="stackTrace">The stacktrace for the exception.</param>
-	protected virtual void WriteLogToDisk(string logString, string stackTrace){
-		
+	protected virtual void WriteLogToDisk (string logString, string stackTrace)
+	{
+
 		#if (UNITY_IPHONE && !UNITY_EDITOR)
 		string logSession = DateTime.Now.ToString("yyyy-MM-dd-HH_mm_ss_fff");
 		string log = logString.Replace("\n", " ");
 		string[]stacktraceLines = stackTrace.Split('\n');
-		
+
 		log = "\n" + log + "\n";
-		foreach (string line in stacktraceLines)
-		{
-			if(line.Length > 0)
-			{
-				log +="  at " + line + "\n";
+		foreach (string line in stacktraceLines) {
+			if (line.Length > 0) {
+				log += "  at " + line + "\n";
 			}
 		}
-		
+
 		List<string> logHeaders = GetLogHeaders();
-		using (StreamWriter file = new StreamWriter(Application.persistentDataPath + LOG_FILE_DIR + "LogFile_" + logSession + ".log", true))
-		{
-			foreach (string header in logHeaders)
-			{
+		using (StreamWriter file = new StreamWriter(Application.persistentDataPath + LOG_FILE_DIR + "LogFile_" + logSession + ".log", true)) {
+			foreach (string header in logHeaders) {
 				file.WriteLine(header);
 			}
 			file.WriteLine(log);
@@ -317,24 +305,21 @@ public class HockeyAppIOS : MonoBehaviour {
 	/// Get the base url used for custom exception reports.
 	/// </summary>
 	/// <returns>A formatted base url.</returns>
-	protected virtual string GetBaseURL() {
-		
-		string baseURL ="";
-		
+	protected virtual string GetBaseURL ()
+	{
+
+		string baseURL = "";
+
 		#if (UNITY_IPHONE && !UNITY_EDITOR)
 
 		string urlString = serverURL.Trim();
-		
-		if(urlString.Length > 0)
-		{
+
+		if (urlString.Length > 0) {
 			baseURL = urlString;
-			
-			if(baseURL[baseURL.Length -1].Equals("/") != true){
+			if (baseURL[baseURL.Length - 1].Equals("/") != true) {
 				baseURL += "/";
 			}
-		}
-		else
-		{
+		} else {
 			baseURL = HOCKEYAPP_BASEURL;
 		}
 		#endif
@@ -343,16 +328,15 @@ public class HockeyAppIOS : MonoBehaviour {
 	}
 
 	/// <summary>
-	/// Get the base url used for custom exception reports.
+	/// Convert selected authentication type to string.
 	/// </summary>
 	/// <returns>A formatted base url.</returns>
-	protected virtual string GetAuthenticatorTypeString() {
-
+	protected virtual string GetAuthenticatorTypeString ()
+	{
 		string authType = "";
 
 		#if (UNITY_IPHONE && !UNITY_EDITOR)
-		switch (authenticatorType)
-		{
+		switch (authenticatorType) {
 		case AuthenticatorType.Device:
 			authType = "BITAuthenticatorIdentificationTypeDevice";
 			break;
@@ -377,18 +361,14 @@ public class HockeyAppIOS : MonoBehaviour {
 	/// <summary>
 	/// Checks whether internet is reachable
 	/// </summary>
-	protected virtual bool IsConnected()
+	protected virtual bool IsConnected ()
 	{
-
 		bool connected = false;
 		#if (UNITY_IPHONE && !UNITY_EDITOR)
-		
-		if  (Application.internetReachability == NetworkReachability.ReachableViaLocalAreaNetwork || 
-		     (Application.internetReachability == NetworkReachability.ReachableViaCarrierDataNetwork))
-		{
+		if  (Application.internetReachability == NetworkReachability.ReachableViaLocalAreaNetwork ||
+		(Application.internetReachability == NetworkReachability.ReachableViaCarrierDataNetwork)) {
 			connected = true;
 		}
-	
 		#endif
 
 		return connected;
@@ -399,8 +379,8 @@ public class HockeyAppIOS : MonoBehaviour {
 	/// </summary>
 	/// <param name="logString">A string that contains the reason for the exception.</param>
 	/// <param name="stackTrace">The stacktrace for the exception.</param>
-	protected virtual void HandleException(string logString, string stackTrace){
-
+	protected virtual void HandleException (string logString, string stackTrace)
+	{
 		#if (UNITY_IPHONE && !UNITY_EDITOR)
 		WriteLogToDisk(logString, stackTrace);
 		#endif
@@ -412,26 +392,27 @@ public class HockeyAppIOS : MonoBehaviour {
 	/// <param name="logString">A string that contains the reason for the exception.</param>
 	/// <param name="stackTrace">The stacktrace for the exception.</param>
 	/// <param name="type">The type of the log message.</param>
-	public void OnHandleLogCallback(string logString, string stackTrace, LogType type){
-		
+	public void OnHandleLogCallback (string logString, string stackTrace, LogType type)
+	{
+
 		#if (UNITY_IPHONE && !UNITY_EDITOR)
-		if(LogType.Assert == type || LogType.Exception == type || LogType.Error == type)	
-		{	
+		if (LogType.Assert == type || LogType.Exception == type || LogType.Error == type) {
 			HandleException(logString, stackTrace);
-		}		
+		}
 		#endif
 	}
 
-	public void OnHandleUnresolvedException(object sender, System.UnhandledExceptionEventArgs args){
-		
+	/// <summary>
+	/// Callback for handling unresolved exceptions.
+	/// </summary>
+	public void OnHandleUnresolvedException (object sender, System.UnhandledExceptionEventArgs args)
+	{
 		#if (UNITY_IPHONE && !UNITY_EDITOR)
-		if(args == null || args.ExceptionObject == null)
-		{	
-			return;	
+		if (args == null || args.ExceptionObject == null) {
+			return;
 		}
 
-		if(args.ExceptionObject.GetType() == typeof(System.Exception))
-		{	
+		if (args.ExceptionObject.GetType() == typeof(System.Exception)) {
 			System.Exception e	= (System.Exception)args.ExceptionObject;
 			HandleException(e.Source, e.StackTrace);
 		}
